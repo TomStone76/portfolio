ada13d62ef98634a7663e725f4800b46afe720b9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-20 23:42:22 -0500

    commit

--- contact.js
+++ contact.js
@@ -1,2 +1,4 @@
+//Test this out, but also research alternatives
+
 var contact = document.getElementById('contactform');
 contactform.setAttribute('action', '//formspree.io/' + 'ts' + '13124' + '@' + 'gmail' + '.' + 'com');
